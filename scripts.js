140b28f0621024329687c5f36e8378ad6cca0ae4
ErikBoesen <me@erikboesen.com>
2016-05-26 08:54:16 -0400

    Video background && git push

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ window.onscroll = function() {
 	if (scroll >= window.innerHeight * 0.65) {
 		page.header.className = 'small';
         page.header.style.opacity = (scroll - window.innerHeight * 0.65) / 50;
-        page.article.style.marginTop = '80vh';
+        page.article.style.marginTop = '100vh';
 	} else {
 		page.header.className = '';
         page.header.style.opacity = 1;
